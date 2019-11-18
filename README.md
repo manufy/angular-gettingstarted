50726800c106845eec417a181d457ede77fc0f56
manufy <manu@tao.homeunix.net>
2019-11-18 10:35:27 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-uymspf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-uymspf)
---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,53 @@
+/* import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+export class ProductAlertsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+} */
+
+/* Step1 import { Component, OnInit } from '@angular/core'; */
+
+/* import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core'; */ 
+
+import { Component } from '@angular/core';
+import { Input } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+
+/*
+export class ProductAlertsComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+} */
+
+/* export class ProductAlertsComponent implements OnInit {
+  @Input() product;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+} */
+
+export class ProductAlertsComponent {
+  @Input() product;
+  @Output() notify = new EventEmitter();
+}
